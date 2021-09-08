cab89fab9b4770c8d12ba9beb7cd4a892ccdbc4d
ninaargade <argade.nina@gmail.com>
2021-09-07 22:02:22 -0700

    includes comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// create table
+// create game board
 
 var body = document.getElementsByTagName("body")[0];    // body variable
 var table = document.createElement("table");            // table variable  
@@ -9,7 +9,7 @@ var tBody = document.createElement("tbody");            // table body variable
 var row = document.createElement("tr");                 // row variable
 var headerRow = tHead.appendChild(row);                 // header row variable
 
-// rows with cells
+// create game board blank spaces
 for (var i = 0; i < 5; i++) {                           // create table rows
 
     var row = document.createElement("tr");             // row variable
@@ -17,8 +17,8 @@ for (var i = 0; i < 5; i++) {                           // create table rows
   for (var j = 0; j < 5; j++) {                         // create table cells
 
     var cell = document.createElement("td");            // cell variable
-    cell.textContent = " ";      
-    cell.style.padding = "20px";                    // create text
+    cell.textContent = " ";                             // create text with blank space
+    cell.style.padding = "20px";                        
     cell.style.width = "20%"
     cell.style.width = "20%"
     cell.style.textAlign = "center"
@@ -36,6 +36,7 @@ body.appendChild(table);                                // append table to body
 
 table.setAttribute("border", "2");   
 
+// create barriers on game board
 let blockedSpaces = []  
 document.getElementById("Cell22").textContent = "X";  
 blockedSpaces.push("Cell22")     
@@ -51,8 +52,8 @@ blockedSpaces.push("Cell54")
 table.style.marginBottom = "30px"
 
 // when page is loaded, select upper left non-header cell
-var currentCell = document.getElementById("Cell11")     // set default currenCell to upper left
-currentCell.style.border = "2px solid black";           // thicker border denotes selction of cell
+var currentCell = document.getElementById("Cell11")     // set default currentCell to upper left
+currentCell.style.border = "2px solid black";           // thicker border denotes selection of cell
 currentCell.style.backgroundColor = "green"; 
 currentCell.textContent = "START";  
 
@@ -504,7 +505,7 @@ var markCellButton = document.createElement("button");
 markCellButton.style.marginBottom = "30px"
 markCellButton.style.width = "5%"
 markCellButton.textContent = "Select";
-markCellButton.setAttribute("id", "mark");
+//markCellButton.setAttribute("id", "mark");
 body.appendChild(markCellButton);
 // button function
 markCellButton.addEventListener("click", function () {      // insert block if space has #
@@ -513,7 +514,7 @@ markCellButton.addEventListener("click", function () {      // insert block if s
     let isBlocked = blockedSpaces.includes(currentCell.id)
     if (isBlocked === false) {
         console.log("not blocked")
-        currentCell.style.backgroundColor = "red";           // change background color of current cell to yellow
+        currentCell.style.backgroundColor = "red";           // change background color of selected cell to red
         currentCell.textContent = "END";
         currentCell.removeAttribute("name")
         currentCell.setAttribute("tag", "end")
@@ -521,54 +522,34 @@ markCellButton.addEventListener("click", function () {      // insert block if s
     
         let endPoint = currentCell.id
 
-        // console.log(endPoint, visitedArr, count_dict, queue)
+        // function to perform Dijkstra
         findPath(endPoint)
     }
-    //setVariables(currentCell.id)
-
 })
 
-//var mycells = document.getElementsByTagName("td")
-//console.log(mycells)
-
-//for (cell of mycells){
-    //console.log(cell)
-    //var value = cell.getAttribute("tag")
-    //console.log(value)
-    //if (value == "end"){
-        //console.log(cell.id)
-        //var endPoint = cell.id
-        //console.log(endPoint)
-
-//function setVariables(endPoint){
         
-
 function findPath (endPoint) {
 
+    // visited cells as breadth-first search is performed
     let visitedArr = [];
         visitedArr.push("Cell11");
-        //console.log(visitedArr)
     
-        // console.log(visitedArr[0])
-    
+    // dictionary to keep track of shortest route to each space
     let count_dict = {}
-    //console.log(count_dict)
-    for (let k = 1; k < 6; k++) {                           // create table rows
-        for (let l = 1; l < 6; l++) {                         // create table cells    
+    for (let k = 1; k < 6; k++) {                           
+        for (let l = 1; l < 6; l++) {                        
             count_dict["Cell" + k + l] = []
         }
     }
-        
-        //console.log(count_dict)
     
+    // queue to perform BFS
     let queue = [];
     queue.push("Cell11");
 
     while (queue.length > 0 && visitedArr.length < 20){
         let cur_node = queue.shift()
         console.log(cur_node)
-        //console.log(cur_node, endPoint)
-        if (cur_node === endPoint){
+        if (cur_node === endPoint){  // at this point, we have reached shortest path, display result
             let result = count_dict[endPoint]
             console.log("result:", result)
             for (let i = 0; i < result.length - 1; i ++){
@@ -577,7 +558,7 @@ function findPath (endPoint) {
         return result
         }
         
-
+        // 4 adjacent nodes to current node
         let adjCoordList = []
         let coord1 = "Cell" + (cur_node[4]) + (cur_node[5] - 1)
         adjCoordList.push(coord1)
@@ -588,24 +569,20 @@ function findPath (endPoint) {
         let coord4 = "Cell" + (parseInt(cur_node[4]) + 1) + (cur_node[5])
         adjCoordList.push(coord4)
 
-        console.log(adjCoordList)
-
         for (let i = 0; i < 4; i++) {
             let coord = adjCoordList[i]
             console.log(coord)
-            if (coord in count_dict){
-                console.log(coord, "hi")
-                if (document.getElementById(coord).textContent == " " || document.getElementById(coord).textContent == "END"){      // not a barrier
-                    console.log("hello")
+            if (coord in count_dict){  // valid space
+                if (document.getElementById(coord).textContent == " " || document.getElementById(coord).textContent == "END"){  // not a barrier
                     console.log(visitedArr)
                     let visited = visitedArr.includes(coord) 
                     console.log(visited)
-                    if (visited === false){
+                    if (visited === false){  // not visited
                         console.log(coord)
-                        queue.push(coord)  
+                        queue.push(coord)  // add to queue
                         console.log(queue)
                         
-
+                        // copy over current path and add current node
                         for (k = 0; k < count_dict[cur_node].length; k++){
                             count_dict[coord].push(count_dict[cur_node][k])
                         }
@@ -615,7 +592,7 @@ function findPath (endPoint) {
 
                         count_dict[coord].push(coord)   
                         console.log(count_dict[coord])             
-                        visitedArr.push(coord)
+                        visitedArr.push(coord)  // mark node as visited
 
                         console.log(count_dict)    
                     }  
